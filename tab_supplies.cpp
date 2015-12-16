480838835aa0407b58801ebcd400a75559463d1a
Martin Goubet <martin-goubet@wanadoo.fr>
2015-12-16 16:58:41 +0100

    COntinue the supplies tab

--- tab_supplies.cpp
+++ tab_supplies.cpp
@@ -4,28 +4,45 @@
 void MainWin::suppliesInit(QSqlDatabase& db)
 {
     QSqlQuery req_supplies(db);
+    req_supplies.prepare("SELECT DISTINCT name_category from Supplies_categories");
+
+    if(req_supplies.exec())
+    {
+        while(req_supplies.next())
+        {
+            ui->tabs_supplies->addTab(new QWidget(ui->tabs_supplies),req_supplies.value(0).toString());
+        }
+    }
+    else
+        qDebug() << req_supplies.lastError();
+
+}
+
+
+void MainWin::suppliesLoad(QSqlDatabase& db)
+{    
+
+    QSqlQuery req_supplies(db);
     QSqlQuery req_AmountSupplies(db);
     int i=1; // put at 0 when we delete the example tab
 
     req_supplies.prepare("SELECT DISTINCT name_category from Supplies_categories");
     req_AmountSupplies.prepare("Select quantity, name_supply "
                                                 " from Stock_camps, Supplies "
-                                                "where Stock_camp.id_camp = :idcourant AND "
-                                                 " Stock_camp.id_supplies = Supplies.id_supplies");
-    req_AmountSupplies.bindValue(":idcourant", m_curCamp);
+                                                "where Stock_camps.id_camp = :id_courant AND "
+                                                 " Stock_camps.id_supplies = Supplies.id_supplies");
+    req_AmountSupplies.bindValue(":id_courant", m_campsIdDb[m_curCamp]);
 
     if(req_supplies.exec())
     {
         while(req_supplies.next())
         {
-            QGridLayout *layout = new QGridLayout;
+            QGridLayout *layout = new QGridLayout();
             QCheckBox *Type;
-             QLineEdit *AmountType ;
+            QLineEdit *AmountType ;
             layout->setColumnStretch(0,0);
             layout->setColumnStretch(1,1);
             layout->setSpacing(0);
-
-            ui->tabs_supplies->addTab(new QWidget(ui->tabs_supplies),req_supplies.value(0).toString());
             ui->tabs_supplies->setCurrentIndex(i);
 
             //Widget for every tabs
@@ -36,6 +53,7 @@ void MainWin::suppliesInit(QSqlDatabase& db)
             {
                 if(req_AmountSupplies.next())
                 {
+                    qDebug() << "enk";
                     Type = new QCheckBox(req_AmountSupplies.value(1).toString());
                     AmountType = new QLineEdit(req_AmountSupplies.value(0).toString());
 
@@ -46,32 +64,15 @@ void MainWin::suppliesInit(QSqlDatabase& db)
                     layout->addWidget(ButtonValidate,2,2);
                     ui->tabs_supplies->currentWidget()->setLayout(layout);
                 }
+                else
+                     qDebug() << req_AmountSupplies.lastQuery();
             }
-
-            /*
-             *while(req_typeSupplies.next())
-             * {
-             *      QCheckBox *Type = new QCheckBox(req_Typesupplies.value(0).toString());
-             *      QLineEdit *AmountType = new QLineEdit(req_typeSupplies.value(1).toString());
-             *
-             * layout->addWidget(Type,y,0);
-             * layout->addWidget(AmountType,y,2);
-             *
-             * } */
-
+            else
+                qDebug() << req_AmountSupplies.lastError();
             i++;
         }
-    //For every tab create put the same configuration in there like quantity,....*/
     }
     else
         qDebug() << req_supplies.lastError();
-
-}
-
-
-void MainWin::suppliesLoad(QSqlDatabase&)
-{
-    /* In function of the maquette */
-    //....
-    //....
+    ui->tabs_supplies->setCurrentIndex(0);
 }
