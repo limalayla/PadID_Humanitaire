77a21ff9c9edfa16e1f0f615d81794f1cc8c085d
Martin Goubet <martin-goubet@wanadoo.fr>
2015-12-16 19:35:00 +0100

    Add a fourth column for the center information

--- tab_supplies.cpp
+++ tab_supplies.cpp
@@ -24,6 +24,7 @@ void MainWin::suppliesLoad(QSqlDatabase& db)
 
     QSqlQuery req_supplies(db);
     QSqlQuery req_AmountSupplies(db);
+    QSqlQuery req_AmountInCenter(db);
     int i=1; // put at 0 when we delete the example tab
     ui->tabs_supplies->clear();
     req_supplies.prepare("SELECT DISTINCT name_category from Supplies_categories ORDER BY id_category");
@@ -34,31 +35,43 @@ void MainWin::suppliesLoad(QSqlDatabase& db)
                                                 " Supplies.id_category = :id_category ");
     req_AmountSupplies.bindValue(":id_courant", m_campsIdDb[m_curCamp]);
 
+    req_AmountInCenter.prepare("Select quantity from Stock_centers, Camps, Supplies"
+                                                    " where Stock_centers.id_center = Camps.id_center AND "
+                                                    " Stock_centers.id_supplies = Supplies.id_supplies AND "
+                                                    " Supplies.id_category = :id_category AND "
+                                                    " Camps.id_camp = :id_courant");
+    req_AmountInCenter.bindValue(":id_courant", m_campsIdDb[m_curCamp]);
+
     if(req_supplies.exec())
     {
         while(req_supplies.next())
         {
             req_AmountSupplies.bindValue(":id_category",i);
+             req_AmountInCenter.bindValue(":id_category",i);
             QGridLayout *layout = new QGridLayout();
             QScrollArea *Area= new QScrollArea();
             QWidget *tab = new QWidget();
             QLabel *Type;
-            QLineEdit *AmountType, *Command;
+            QLineEdit *AmountType, *Command, *AmountInCenter;
 
             //Widget for every tabs
             QLabel *NomTab = new QLabel(req_supplies.value(0).toString());
             QLabel *Amount = new QLabel(tr("Amount"));
             QLabel *LabelCommand = new QLabel(tr("Amount to command"));
             QPushButton *ButtonValidate = new QPushButton(tr("Confirm"));
+            QLabel *LabelAmountInCenter = new QLabel(tr("Amount in Center"));
 
             layout->addWidget(NomTab,0,0);
             layout->addWidget(Amount,0,1);
-            layout->addWidget(LabelCommand,0,2);
+            layout->addWidget(LabelAmountInCenter,0,2);
+            layout->addWidget(LabelCommand,0,3);
+
             int y=1;
             if(req_AmountSupplies.exec())
             {
                 while(req_AmountSupplies.next())
                 {
+
                     Type = new QLabel(req_AmountSupplies.value(1).toString());
                     AmountType = new QLineEdit(req_AmountSupplies.value(0).toString());
                     Command = new QLineEdit();
@@ -66,13 +79,25 @@ void MainWin::suppliesLoad(QSqlDatabase& db)
 
                     layout->addWidget(Type,y,0);
                     layout->addWidget(AmountType,y,1);
-                    layout->addWidget(Command,y,2);
+                    layout->addWidget(Command,y,3);
+
+                    if(req_AmountInCenter.exec())
+                    {
+                        while(req_AmountInCenter.next())
+                        {
+                            AmountInCenter=new QLineEdit(req_AmountInCenter.value(0).toString());
+                            AmountInCenter->setEnabled(false);
+                            layout->addWidget(AmountInCenter,y,2);
+                        }
+                    }
+                    else
+                        qDebug() << req_AmountInCenter.lastError();
                     y++;
                 }
             }
             else
                 qDebug() << req_AmountSupplies.lastError();
-            layout->addWidget(ButtonValidate,y,3);
+            layout->addWidget(ButtonValidate,y,4);
             tab->setLayout(layout);
             Area->setWidget(tab);
             ui->tabs_supplies->addTab(Area,req_supplies.value(0).toString());
